1d8dda36c2d2bf8525355027e4b0f7ed0d3f93af
mirkg <mirkg@github>
2026-02-16 13:48:39 +0100

    Fix devices mapping error

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 """
-<plugin key="WifiSensors" name="WifiSensors" version="0.1.0">
+<plugin key="WifiSensors" name="WifiSensors" version="0.1.1">
     <description>
         <h2>WifiSensors Plugin</h2>
         <p>Plugin to add support for <a href="https://github.com/mirkg/arduino-WifiSensors"> WifiSensors </a> project</p>
